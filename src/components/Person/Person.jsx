2483d349a25db7684b8e33b4f7e3de3c34ab68c7
diego69-foda <diego.bkuhn@gmail.com>
2025-09-03 14:42:23 -0300

    com correções recomendadas

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,12 +1,14 @@
 // import cn from 'classnames';
 
-export const Person = ({ person }) => (
+export const Person = ({ name,age = null,sex,isMarried,partnerName }) => (
   <section className="Person">
-    <h2 className="Person__name">My name is {person.name}</h2>
-    <p className="Person__age">I am {person.age}</p>
-    {person.isMarried ? (
+    <h2 className="Person__name">My name is {name}</h2>
+    {age === null ? (
+      <p className="Person__age">I am {age}</p>) : (
+      '')}
+    {isMarried ? (
       <p className="Person__partner">
-        {person.partnerName} is my {person.sex === 'm' ? 'wife' : 'husband'}
+        {partnerName} is my {sex === 'm' ? 'wife' : 'husband'}
       </p>
     ) : (
       <p>I am not married</p>
