7ac13448a790a8ac67b7d4915a22048152b584b1
diego69-foda <diego.bkuhn@gmail.com>
2025-09-03 15:23:39 -0300

    mudanças

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,13 +1,11 @@
 // import cn from 'classnames';
 
-export const Person = person => (
+export const Person = (person) => (
   <section className="Person">
     <h2 className="Person__name">My name is {person.name}</h2>
     {person.age !== undefined ? (
-      <p className="Person__age">I am {person.age}</p>
-    ) : (
-      ''
-    )}
+      <p className="Person__age">I am {person.age}</p>) : (
+      '')}
     {person.isMarried ? (
       <p className="Person__partner">
         {person.partnerName} is my {person.sex === 'm' ? 'wife' : 'husband'}
